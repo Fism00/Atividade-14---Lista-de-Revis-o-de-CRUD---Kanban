aae01ced616a8444e665e9771805fe3ed335aa99
Mordecai <you@example.com>
2025-10-31 08:41:12 -0300

    add - cadastro criado

---
+++ public/cadastro.php
@@ -0,0 +1,53 @@
+<?php
+
+include '../config/db.php';
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $senha = $_POST['senha'];
+
+    $sql = " INSERT INTO usuario (nome,email,senha) VALUE ('$name','$email','$senha')";
+
+    if ($conn->query($sql) === true) {
+        echo "Novo usuario criado com sucesso.";
+    } else {
+        echo "Erro " . $sql . '<br>' . $conn->error;
+    }
+    $conn->close();
+}
+
+?>
+
+
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Create</title>
+</head>
+
+<body>
+
+    <form method="POST" action="cadastro.php">
+
+        <label for="name">Nome:</label>
+        <input type="text" name="name" required>
+
+        <label for="email">email:</label>
+        <input type="text" name="email">
+
+        <label for="senha">senha: </label>
+        <input type="password" name="senha" required>
+
+        <input type="submit" value="criar usuario">
+
+    </form>
+
+    <a href="READ_kanban.php">Ver kanban.</a>
+
+</body>
+
+</html>
